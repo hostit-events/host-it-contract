f870dc763f0c8e609ed9ba1399700a9290efec15
David Dada <daveproxy80@gmail.com>
2025-02-22 12:33:53 +0100

    feat: add itc and event factory events

--- contracts/libraries/constants/Logs.sol
+++ contracts/libraries/constants/Logs.sol
@@ -2,9 +2,23 @@
 pragma solidity ^0.8.0;
 
 import {LibApp} from "../LibApp.sol";
+import {Types} from "./Types.sol";
 
 library Logs {
+    // =============================================================
+    //                   EVENT STORAGE EVENTS/LOGS
+    // =============================================================
+    event EventCreated(uint256 eventId, Types.EventData eventData);
+    event EventUpdated(uint256 eventId, Types.EventData eventData);
+    event AttendeeRegistered(uint256 eventId, address attendee);
+    event AttendeeCheckedIn(uint256 eventId, address attendee);
+
+    // =============================================================
+    //                      MANUAL EVENTS/LOGS
+    // =============================================================
     event AttendedW3LC2024(address indexed attendee, LibApp.AttendanceDay indexed day, uint256 time);
     event AttendedAW3C2024(address indexed attendee, LibApp.AttendanceDay indexed day, uint256 time);
     event AttendedBDRLSC2024(address indexed attendee, LibApp.AttendanceDay indexed day, uint256 time);
+    event AttendedBIUC2024(address indexed attendee, LibApp.AttendanceDay indexed day, uint256 time);
+    event AttendedITC2024(address indexed attendee, LibApp.AttendanceDay indexed day, uint256 time);
 }
