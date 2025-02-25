9c8e5cababdb064c534fc5bc005c84b5ddb4d2dd
David Dada <daveproxy80@gmail.com>
2025-02-25 03:33:21 +0100

    chore: rename folder

---
+++ contracts/facets/standalone_events/W3LC2024Facet.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {LibDiamond} from "contracts/libraries/LibDiamond.sol";
+import {LibApp} from "contracts/libraries/LibApp.sol";
+import {Errors} from "contracts/libraries/constants/Errors.sol";
+import {Logs} from "contracts/libraries/constants/Logs.sol";
+import {IERC721AUpgradeable} from "erc721a-upgradeable/contracts/IERC721AUpgradeable.sol";
+import {W3LC2024} from "contracts/nfts/W3LC2024.sol";
+
+contract W3LC2024Facet {
+    /**
+     * @dev Sets the address for W3LC2024 NFT ticket to `_w3lc2024nft`.
+     *
+     * Requirements:
+     *
+     * - msg.sender must have the role `W3LC3_ADMIN_ROLE`.
+     */
+    function setW3LC2024NFT(address _w3lc2024nft) external {
+        LibDiamond._checkRole(LibDiamond.W3LC3_ADMIN_ROLE);
+        LibApp.AppStorage storage s = LibApp.appStorage();
+
+        s.W3LC2024NFT = _w3lc2024nft;
+    }
+
+    /**
+     * @dev Toggles the day of attendance `day` to true.
+     *
+     * Requirements:
+     *
+     * - msg.sender must have the role `W3LC3_ADMIN_ROLE`.
+     */
+    function w3lc2024__setDayActive(LibApp.AttendanceDay day) external {
+        LibDiamond._checkRole(LibDiamond.W3LC3_ADMIN_ROLE);
+        LibApp.AppStorage storage s = LibApp.appStorage();
+
+        s.w3lc2024_isDayActive[day] = true;
+    }
+
+    /**
+     * @dev Toggles the day of attendance `day` to false.
+     *
+     * Requirements:
+     *
+     * - msg.sender must have the role `W3LC3_ADMIN_ROLE`.
+     */
+    function w3lc2024__setDayInactive(LibApp.AttendanceDay day) external {
+        LibDiamond._checkRole(LibDiamond.W3LC3_ADMIN_ROLE);
+        LibApp.AppStorage storage s = LibApp.appStorage();
+
+        s.w3lc2024_isDayActive[day] = false;
+    }
+
+    /**
+     * @dev Marks the attendance of an `attendee` for the `day` to true.
+     *
+     * Also stores the `attendee` in an attendees array for the `day`
+     *
+     * Requirements:
+     *
+     * - `msg.sender` must have the role `W3LC3_ADMIN_ROLE`.
+     * - `day` must be active.
+     * - `attendee` must not be registered already for that `day`.
+     * - `attendee` must have a W3LC2024 NFT ticket.
+     *
+     * Emits an {AttendedW3LC2024} event.
+     */
+    function w3lc2024__markAttendance(address attendee, LibApp.AttendanceDay day) external {
+        LibDiamond._checkRole(LibDiamond.W3LC3_ADMIN_ROLE);
+        LibApp.AppStorage storage s = LibApp.appStorage();
+
+        if (s.w3lc2024_isDayActive[day] == false) revert Errors.W3LC2024__DayNotActive(uint8(day));
+
+        if (s.w3lc2024_attended[attendee][day] == true) revert Errors.W3LC2024__AlreadyAttended(uint8(day));
+
+        if (IERC721AUpgradeable(s.W3LC2024NFT).balanceOf(attendee) == 0) W3LC2024(s.W3LC2024NFT).mintSingle(attendee);
+
+        if (day == LibApp.AttendanceDay.Day1) {
+            s.w3lc2024_day1Attendees.push(attendee);
+        } else if (day == LibApp.AttendanceDay.Day2) {
+            s.w3lc2024_day2Attendees.push(attendee);
+        } else if (day == LibApp.AttendanceDay.Day3) {
+            s.w3lc2024_day3Attendees.push(attendee);
+        } else {
+            revert Errors.W3LC2024__InvalidDay(uint8(day));
+        }
+
+        s.w3lc2024_attended[attendee][day] = true;
+
+        emit Logs.AttendedW3LC2024(attendee, day, block.timestamp);
+    }
+
+    /**
+     * @dev Returns if the attendance of an `attendee` for the `day` is true.
+     *
+     * Requirements:
+     *
+     * - `msg.sender` must have the role `W3LC3_ADMIN_ROLE`.
+     */
+    function w3lc2024__verifyAttendance(address attendee, LibApp.AttendanceDay day) public view returns (bool) {
+        LibApp.AppStorage storage s = LibApp.appStorage();
+
+        return s.w3lc2024_attended[attendee][day];
+    }
+
+    /**
+     * @dev Returns array of marked `attendee`s for all the `day`s.
+     *
+     */
+    function w3lc2024__returnAttendance() external view returns (address[] memory day1, address[] memory day2, address[] memory day3) {
+        LibApp.AppStorage storage s = LibApp.appStorage();
+
+        day1 = s.w3lc2024_day1Attendees;
+        day2 = s.w3lc2024_day2Attendees;
+        day3 = s.w3lc2024_day3Attendees;
+    }
+
+    /**
+     * @dev Returns if day is active.
+     *
+     */
+    function w3lc2024__isDayActive(LibApp.AttendanceDay day) external view returns (bool) {
+        LibApp.AppStorage storage s = LibApp.appStorage();
+
+        return s.w3lc2024_isDayActive[day];
+    }
+}
---
+++ contracts/nfts/EventERC721A.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC721AUpgradeable} from "./ERC721AUpgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+
+/**
+ * @title EventNFT
+ * @author HostIT (David Dada, Manoah Luka, Olorunsogo Banwo, and Oluwakemi Atoyebi)
+ *
+ * EventNFT Contract to mint tickets to attendees
+ */
+contract EventERC721A is ERC721AUpgradeable, AccessControlUpgradeable, PausableUpgradeable {
+    // =============================================================
+    //                       INITIALIZER LOGIC
+    // =============================================================
+
+    function initialize(address _admin, string calldata _name, string calldata _symbol) public initializerERC721A initializer {
+        __ERC721A_init(_name, _symbol);
+        __AccessControl_init();
+        __Pausable_init();
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+    }
+
+    // =============================================================
+    //                       NFT METADATA
+    // =============================================================
+
+    /**
+     * @dev Sets the Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`.
+     */
+    function setBaseURI(string memory _uri) external {
+        _checkRole(DEFAULT_ADMIN_ROLE);
+        _setBaseURI(_uri);
+    }
+
+    // =============================================================
+    //                         MINT FUNCTIONS
+    // =============================================================
+
+    /**
+     * @dev Mints `1` token of the current tokenId and transfers them to `to`.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     */
+    function mintOne(address to) external {
+        _checkRole(DEFAULT_ADMIN_ROLE);
+        _mint(to, 1);
+    }
+
+    /**
+     * @dev Mints `quantity` tokens and transfers them to `to`.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - `quantity` must be greater than 0.
+     *
+     * Emits a {Transfer} event for each mint.
+     */
+    function mintMultipe(address to, uint256 quantity) external {
+        _checkRole(DEFAULT_ADMIN_ROLE);
+        _mint(to, quantity);
+    }
+
+    // =============================================================
+    //                            IERC165
+    // =============================================================
+
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * [EIP section](https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified)
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721AUpgradeable, AccessControlUpgradeable) returns (bool) {
+        // The interface IDs are constants representing the first 4 bytes
+        // of the XOR of all function selectors in the interface.
+        // See: [ERC165](https://eips.ethereum.org/EIPS/eip-165)
+        // (e.g. `bytes4(i.functionA.selector ^ i.functionB.selector ^ ...)`)
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 interface ID for ERC165.
+            interfaceId == 0x80ac58cd || // ERC165 interface ID for ERC721.
+            interfaceId == 0x5b5e139f || // ERC165 interface ID for ERC721Metadata.
+            interfaceId == type(IAccessControl).interfaceId || // ERC165 interface ID for AccessControl.
+            super.supportsInterface(interfaceId);
+    }
+}
+
+// t>6cXt+Y=JccMMiM~'i:tJcNXi'.'~''~:::::::::::::~:~~~~~~~~~~~~~~~~~~~~'~''
+// +>+>+>i+: '+MM5+      'YDc:.~~~~~::::;;:;;::::~:::::~~~~'.          .'~~
+// iiiiiiii+~.>KMMH+':!+=JD6=''~~~~:::::::::::::::~:~:~~'.                '
+// ccci===i=; .iMMMMMMMMMMQc: ~;!:::::::::::::::;:::::~'   '>=+;!>++>!:
+// ijcii==++>~ ~jMMMMMMMNSi~ .;!;:::;;;;:;;;;:;;;;;::~'  '>j5YYY565YYYJj+~
+// cjjci===++!~ .;+icccc+~   ~;;:;;;;!!!!!;!;!!;;;:::'. ~=Y66665Y5665YY55j;
+// cJttii=+>>>>;~.         '::;;!!!;;!!!!!!;;;;;;;;:~. '+Y665Yt>  :=J55666c
+// cJJttcc=>>!!!;;:~''~::;;;;;:;;;;!;!!!!!!!!;;!;;;:~. :i5S556t!   '=Y566St
+// =jYYJJti=+!!;;::;!!>!!;;::;;;;;:;;;;;;;;;;;;;;;;:~  :=5S55Yj>  .+t55SSt;
+// =jY6S6Ytci>;;;::::~~~~:::;:::::::;;!:;;;;:;;;;;;::'  !tXS666Jtt5SDDSJ+
+// +itSNQSYji+>;;;;;;::::::::~::;;;;;;;!!;!;!;;;;!;;;:'  :=JSDKQKKQQ5j!.
+// !+=jDMQtji>;;;;:::;:::::~:~~::::::;;;;;;;;!!!;;!;:::'    .:!++>;~     .~
+// !;;;=MMMNti+>!;;;:;;::~:~~~~~~~::::::::;;;;;!!!!;;!;;::'            ':::
+// >;;~':iMMMM5j=>;:::::::::~~~~~~~:~~:::::::;;;;;;;;;;;:;:::~;!!>>>;;:::::
+// +!;:~.  ~MMMM6Jc=>;;:;;:::::::::::~~~::::::::;;;:;::;:::::::::~:::::::::
+// c+!;:~~  ;MMMMMSi+;;:;;;;;;;:::::::::::::;;;:::::;;;::;;::::::::::::::::
+// Yc+!;::~   iMMMMMMMc!:;;!!;;!;;;;;:;:::::::::;;;;::;::;:;:::::::::::::::
+// QJc+!::~''   >MMMMMMMKYc++==+>!!;;;;;;;;::::::::;;;:;;:;;:::::::::::::::
+// MM6j=!::~''.     '=tXNHSYJJYt==+!!;!;:::::::::::::;:;;;;:;::::::::::::::
+// MMMXc=!:~~''''          ':!+===>+>!!;::::::~::::~::::::::::::::::;::::~:
+// MMMMMt>::~~~~''.    .~:'    ':;;!!;;!!;;;::::~~~~~~~~~~:~::::::::~~:::::
+// MMMMMMJ!:::~~~''.   .~;!>>:.    :;:::~:;;;;;;;:::::~~''''''~~~~~~:~~::::
+// MMMMMMMc::~~''''..     .':!!;'''                            '''''~~~~~::
+// MMMMMMMM>~~::~~''...         .::;!!!;:.                    .'''~~:~::;;;
+// MMMMMMMMM:~~~~~~''''....                              ...''''~~::::;::;:
+// MMMMMMMMMN~~~~'''''''''....  .               . .    ..'''''~~::::;:::;::
+// MMMMMMMMMMX'~::~~~~'~~~~'''''''..........'''''.....'~~~:::;;;;;;;;;:;;;;
+// MMMMMMMMMMMN!;::~~~'''~~~~''~~~~~'''''''''''..'''~~::;;;;;:;;;;;:;;;:;;;
+/////////////////////////////THANKS FOR COMING/////////////////////////////
